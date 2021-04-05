e1abb959ee7523e203a09199bf6a23ae9fbfcae9
Tamara Spivey <tjsxz4@mst.edu>
2021-04-04 20:26:50 -0800

    added java directory

---
+++ Python/NewYearsChaos.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def pair_testing():
+    print('placeholder')
+
+
+# Complete the minimumBribes function below.
+# def minimumBribes(q):
+#     print("placeholder")
+#
+#
+# if __name__ == '__main__':
+#     t = int(input())
+#
+#     for t_itr in range(t):
+#         n = int(input())
+#
+#         q = list(map(int, input().rstrip().split()))
+#
+#         minimumBribes(q)
